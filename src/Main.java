24851ee94893244ae223265cc123a5c64a02b1cc
hsd9681 <hsd9681@naver.com>
2024-04-25 17:11:45 +0900

    반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다

--- src/Main.java
+++ src/Main.java
@@ -5,38 +5,45 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    String text = "";
     int result = 0;
-    System.out.print("첫 번째 숫자를 입력하세요: ");
-    int num1 = sc.nextInt();
-    System.out.print("사칙연산을 입력하세요: ");
-    sc.nextLine();
-    String mid = sc.nextLine();
-    System.out.print("두 번째 숫자를 입력하세요: ");
-    int num2 = sc.nextInt();
-    sc.nextLine();
 
-    switch (mid) {
-      case "+":
-        result = num1 + num2;
-        System.out.println("결과: " + result);
+    while (!text.equals("exit")) {
+      System.out.print("첫 번째 숫자를 입력하세요: ");
+      int num1 = sc.nextInt();
+      System.out.print("사칙연산을 입력하세요: ");
+      sc.nextLine();
+      String mid = sc.nextLine();
+      System.out.print("두 번째 숫자를 입력하세요: ");
+      int num2 = sc.nextInt();
+      sc.nextLine();
 
-        break;
-      case "-":
-        result = num1 - num2;
-        System.out.println("결과: " + result);
-        break;
-      case "*":
-        result = num1 * num2;
-        System.out.println("결과: " + result);
-        break;
-      case "/":
-        if (num2 != 0) {
-          result = num1 / num2;
+      switch (mid) {
+        case "+":
+          result = num1 + num2;
           System.out.println("결과: " + result);
-        } else {
-          System.out.println("0으로 나눌 수 없습니다.");
-        }
-        break;
+
+          break;
+        case "-":
+          result = num1 - num2;
+          System.out.println("결과: " + result);
+          break;
+        case "*":
+          result = num1 * num2;
+          System.out.println("결과: " + result);
+          break;
+        case "/":
+          if (num2 != 0) {
+            result = num1 / num2;
+            System.out.println("결과: " + result);
+          } else {
+            System.out.println("0으로 나눌 수 없습니다.");
+          }
+          break;
+      }
+      System.out.print("onemore?");
+      text = sc.nextLine(); // 사용자 입력을 text 변수에 할당
     }
+    System.out.println("프로그램을 종료합니다.");
   }
 }
