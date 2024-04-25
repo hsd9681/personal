d25de3ad6bc3edc54dff8eb9a63c081a3bc12b58
hsd9681 <hsd9681@naver.com>
2024-04-25 17:15:22 +0900

    5. 연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.     - 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다.     - 연산의 결과를 비어있는 곳에 저장하기 위해 저장할 때마다 count 합니다.

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     String text = "";
     int result = 0;
+    int Array[] = new int[10];
+    int arrayindex = 0;
 
     while (!text.equals("exit")) {
       System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -22,28 +24,35 @@ public class Main {
         case "+":
           result = num1 + num2;
           System.out.println("결과: " + result);
-
+          Array[arrayindex] = result;
           break;
         case "-":
           result = num1 - num2;
           System.out.println("결과: " + result);
+          Array[arrayindex] = result;
           break;
         case "*":
           result = num1 * num2;
           System.out.println("결과: " + result);
+          Array[arrayindex] = result;
           break;
         case "/":
           if (num2 != 0) {
             result = num1 / num2;
             System.out.println("결과: " + result);
+            Array[arrayindex] = result;
           } else {
             System.out.println("0으로 나눌 수 없습니다.");
           }
           break;
       }
+      arrayindex++;
       System.out.print("onemore?");
       text = sc.nextLine(); // 사용자 입력을 text 변수에 할당
     }
     System.out.println("프로그램을 종료합니다.");
+    for(int i =0; i<Array.length; i++){
+      System.out.println(Array[i]);
+    }
   }
 }
