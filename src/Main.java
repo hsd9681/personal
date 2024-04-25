fb778b5bf67c34a9741e0677779f832398abcced
hsd9681 <hsd9681@naver.com>
2024-04-25 17:05:04 +0900

    양의정수2개입력받기

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
   public static void main(String[] args) {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
+    Scanner sc = new Scanner(System.in);
+    System.out.print("첫 번째 숫자를 입력하세요: ");
+    int num1 = sc.nextInt();
+    System.out.print("두 번째 숫자를 입력하세요: ");
+    int num2 = sc.nextInt();
+    sc.nextLine();
 
-
-    for (int i = 1; i <= 5; i++) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-      System.out.println("i = " + i);
-    }
   }
 }
