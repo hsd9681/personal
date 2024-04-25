443c5a2f1ce48563ce2a920a5fe1f82249ac9556
hsd9681 <hsd9681@naver.com>
2024-04-25 17:08:14 +0900

    입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력합니다

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int result = 0;
     System.out.print("첫 번째 숫자를 입력하세요: ");
     int num1 = sc.nextInt();
     System.out.print("사칙연산을 입력하세요: ");
@@ -14,5 +15,28 @@ public class Main {
     int num2 = sc.nextInt();
     sc.nextLine();
 
+    switch (mid) {
+      case "+":
+        result = num1 + num2;
+        System.out.println("결과: " + result);
+
+        break;
+      case "-":
+        result = num1 - num2;
+        System.out.println("결과: " + result);
+        break;
+      case "*":
+        result = num1 * num2;
+        System.out.println("결과: " + result);
+        break;
+      case "/":
+        if (num2 != 0) {
+          result = num1 / num2;
+          System.out.println("결과: " + result);
+        } else {
+          System.out.println("0으로 나눌 수 없습니다.");
+        }
+        break;
+    }
   }
 }
